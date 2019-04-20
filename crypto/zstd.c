8b98e60f1a0bcb1d434dc8929a595744a81c81f2
DevriesL <therkduan@gmail.com>
2019-04-20 22:33:12 +0800

    zstd: Merge optimizations from Samsung
    [ YumeMichi: Forward port to latest zstd source. See facebook/zstd@fa3671e. ]
    
    Signed-off-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: Sean Du <do4suki@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- crypto/zstd.c
+++ crypto/zstd.c
@@ -14,8 +14,12 @@
 #include <linux/zstd.h>
 #include <crypto/internal/scompress.h>
 
-uint compression_level = 2;
-module_param(compression_level, uint, 0644);
+<<<<<<< HEAD
+#define ZSTD_DEF_LEVEL	1
+=======
+
+#define ZSTD_DEF_LEVEL	1
+>>>>>>> 21b86989e90d (zstd: Merge optimizations from Samsung)
 
 struct zstd_ctx {
 	zstd_cctx *cctx;
@@ -26,11 +30,11 @@ struct zstd_ctx {
 
 static zstd_parameters zstd_params(void)
 {
-	if (compression_level == 0)
-		compression_level = 1;
-	if (compression_level > zstd_max_clevel())
-		compression_level = zstd_max_clevel();
-	return zstd_get_params(compression_level, PAGE_SIZE);
+<<<<<<< HEAD
+	return zstd_get_params(ZSTD_DEF_LEVEL, PAGE_SIZE);
+=======
+	return zstd_get_params(ZSTD_DEF_LEVEL, PAGE_SIZE);
+>>>>>>> 21b86989e90d (zstd: Merge optimizations from Samsung)
 }
 
 static int zstd_comp_init(struct zstd_ctx *ctx)
